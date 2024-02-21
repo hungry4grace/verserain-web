1eefb0dbc91f14f9158ee1f53131b8f4d9c47c95
Samuel Hsiung <hsiung@gmail.com>
2024-02-21 04:05:43 +0000

    min word for email

--- server/base/base.py
+++ server/base/base.py
@@ -39,11 +39,12 @@ class ContactHandler(BaseHandler):
         reply_to_email = self.get_argument("email")
         to_email = ["help@%s" % settings.MAIL_DOMAIN] # must be a list
         subject = self.get_argument('subject', '')
-        
-        message = self.get_argument('message', '')
-        message += "\n\nemail: %s" % (self.current_user.get('email') or '')
-        message += "\nuser info: %s" % self.current_user
-        
-        EmailQueue.queue_mail(from_email,to_email,subject,message,reply_to=reply_to_email)
+        body = self.get_argument('message', '')
+        num_words = len(body.split())
+        if num_words >= 5:
+            message = self.get_argument('message', '')
+            message += "\n\nemail: %s" % (self.current_user.get('email') or '')
+            message += "\nuser info: %s" % self.current_user
+            EmailQueue.queue_mail(from_email,to_email,subject,message,reply_to=reply_to_email)
 
         self.redirect("/contact?message_sent=True")
