480a9cfed69bb92f1d01b751570d3c8ffb9bac2d
David Hwang <hungry4grace@gmail.com>
2026-04-13 10:57:44 +0800

    fix(custom-set): resolve crash from react-quill on React 19

--- verserain-web/src/App.jsx
+++ verserain-web/src/App.jsx
@@ -6,8 +6,8 @@ import PartySocket from 'partysocket';
 import QRCode from 'qrcode';
 import './index.css';
 import { BIBLE_BOOKS } from './bibleDictionary';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 const quillModules = {
   toolbar: [
