f5b99f42d2e9b6e5db556bbc90c6af5c7f90ba57
David Hwang <hungry4grace@users.noreply.github.com>
2026-05-17 09:37:08 +0800

    Bump version to 3.6.0

--- verserain-web/src/App.jsx
+++ verserain-web/src/App.jsx
@@ -6138,7 +6138,7 @@ const deDict = {
                     verserain
                   </div>
                   <div style={{ fontSize: '0.65rem', color: '#94a3b8', fontWeight: 'bold', letterSpacing: '1px', marginTop: '4px', marginLeft: '2px' }}>
-                    v3.5.6
+                    v3.6.0
                   </div>
                 </div>
                 <select
